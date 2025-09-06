6ea9a3ee15f0ce0894ed54110a599dee6062a724
ayomikun-adekeye <ayomikunadekeye7@gmail.com>
2025-09-06 17:17:30 +0100

    Create bank.py

---
+++ WEEK1/bank.py
@@ -0,0 +1,9 @@
+greeting = input("Greating please: ")
+greeting = greeting.strip().lower()
+
+if greeting[0:5] == 'hello':
+    print("$0")
+elif greeting[0] == 'h':
+    print("$20")
+else:
+    print("$100")
